attention backprop but its wrong

attention.py
@@ -27,7 +27,15 @@ class Attention(Layer):
         self.g: np.typing.NDArray = np.ones((contextSize, embedDim))
         self.b: np.typing.NDArray = np.zeros((contextSize, embedDim))
 
+        self.qkvError = np.zeros((embedDim, embedDim * 3))
+        self.projError = np.zeros((embedDim, embedDim))
+
+        self.gError: np.typing.NDArray = np.zeros((contextSize, embedDim))
+        self.bError: np.typing.NDArray = np.zeros((contextSize, embedDim))
+        self.error = np.zeros((contextSize, embedDim))
+
     def feedForward(self, lastLayer: np.typing.NDArray):
+        self.input = lastLayer
         q, k, v = [
             np.split(x, self.headCount, axis=1)
             for x in np.split(lastLayer @ self.qkv, 3, axis=1)
@@ -40,4 +48,60 @@ class Attention(Layer):
             self.heads[i].feedForward(lastLayer)
             attentionOutputs.append(self.heads[i].a)
 
-        self.a, *_ = layerNorm(np.hstack(attentionOutputs) @ self.proj, self.g, self.b)
+        self.combined = np.hstack(attentionOutputs)
+        self.a, self.z, self.mean, self.var = layerNorm(
+            self.combined @ self.proj, self.g, self.b
+        )
+
+    def backProp(self, error: np.typing.NDArray):
+        self.bError += error
+        self.gError += error * self.z
+        # derivative of layer norm
+        n = error.shape[-1]
+        stdev = np.sqrt(self.var + 1e-5)
+        error *= (
+            self.g
+            * (1 / n * stdev)
+            * (n - 1 - (self.combined - self.mean) ** 2 / self.var)
+        )
+
+        self.projError += self.combined.T @ error
+
+        splitError = np.split(error @ self.proj.T, self.headCount, axis=1)
+        # print(splitError[0].shape)
+        qkvErrors = [[], [], []]
+        for i in range(len(self.heads)):
+            self.heads[i].backProp(splitError[i])
+            qkvErrors[0].append(self.heads[i].queryError)
+            qkvErrors[1].append(self.heads[i].keyError)
+            qkvErrors[2].append(self.heads[i].valueError)
+            # qkvErrors[0].append(
+            #     np.zeros((self.contextSize, self.embedDim // self.headCount))
+            # )
+            # qkvErrors[1].append(
+            #     np.zeros((self.contextSize, self.embedDim // self.headCount))
+            # )
+            # qkvErrors[2].append(
+            #     np.zeros((self.contextSize, self.embedDim // self.headCount))
+            # )
+
+        error = np.hstack([np.hstack(x) for x in qkvErrors])
+        self.qkvError += self.input.T @ error
+        # print(error.shape, self.qkv.shape)
+        self.error += error @ self.qkv.T
+
+    def gradientDescent(self, learningRate: float, batchSize: int):
+        self.b -= self.bError * learningRate / batchSize
+        self.g -= self.gError * learningRate / batchSize
+        self.proj -= self.projError * learningRate / batchSize
+        self.qkv -= self.qkvError * learningRate / batchSize
+
+        self.qkvError = np.zeros((self.embedDim, self.embedDim * 3))
+        self.projError = np.zeros((self.embedDim, self.embedDim))
+
+        self.gError: np.typing.NDArray = np.zeros((self.contextSize, self.embedDim))
+        self.bError: np.typing.NDArray = np.zeros((self.contextSize, self.embedDim))
+        self.error = np.zeros((self.contextSize, self.embedDim))
+
+        # for head in self.heads:
+        #     head.gradientDescent(learningRate, batchSize)

attentionHead.py
@@ -9,6 +9,7 @@ class AttentionHead(Layer):
     def __init__(
         self, contextSize: int, embedDim: int, headCount: int, mask: np.typing.NDArray
     ) -> None:
+        self.contextSize = contextSize
         self.embedDim = embedDim
         self.headCount = headCount
         self.mask = mask
@@ -20,6 +21,16 @@ class AttentionHead(Layer):
         self.key: np.typing.NDArray = np.empty((contextSize, embedDim // headCount))
         self.value: np.typing.NDArray = np.empty((contextSize, embedDim // headCount))
         self.a = np.empty((contextSize, embedDim))
+
+        self.queryError: np.typing.NDArray = np.empty(
+            (contextSize, embedDim // headCount)
+        )
+        self.keyError: np.typing.NDArray = np.empty(
+            (contextSize, embedDim // headCount)
+        )
+        self.valueError: np.typing.NDArray = np.empty(
+            (contextSize, embedDim // headCount)
+        )
 
     def feedForward(self, lastLayer: np.typing.NDArray):
         attentionPattern = np.where(
@@ -34,7 +45,7 @@ class AttentionHead(Layer):
             -np.inf,
         ) / (np.sqrt(self.embedDim))
 
-        weights = softmax(attentionPattern)
+        self.weights = softmax(attentionPattern)
         # value = np.matmul(lastLayer, np.matmul(self.valueUp, self.valueDown))
         # print("alskdjf")
         # change = (
@@ -42,4 +53,25 @@ class AttentionHead(Layer):
         #     * weights.reshape(contextSize, contextSize, 1)
         # ).sum(1)
 
-        self.a = weights @ self.value
+        self.a = self.weights @ self.value
+
+    def backProp(self, error: np.typing.NDArray):
+        self.valueError = self.weights.T @ error
+        error = np.where(
+            self.mask,
+            (error @ self.value.T) * self.weights * (1 - self.weights),
+            0,
+        ) / (np.sqrt(self.embedDim))
+        self.queryError = error @ self.key
+        self.keyError = (self.query.T @ error).T
+
+    def gradientDescent(self, learningRate: float, batchSize: int):
+        self.queryError: np.typing.NDArray = np.empty(
+            (self.contextSize, self.embedDim // self.headCount)
+        )
+        self.keyError: np.typing.NDArray = np.empty(
+            (self.contextSize, self.embedDim // self.headCount)
+        )
+        self.valueError: np.typing.NDArray = np.empty(
+            (self.contextSize, self.embedDim // self.headCount)
+        )

embedding.py
@@ -36,8 +36,8 @@ class Embedding(Layer):
         self.error += error @ self.words
 
     def gradientDescent(self, learningRate: float, batchSize: int):
-        self.words -= self.wordsError * learningRate / batchSize
-        self.positions -= self.positionsError * learningRate / batchSize
+        # self.words -= self.wordsError * learningRate / batchSize
+        # self.positions -= self.positionsError * learningRate / batchSize
         self.error = np.zeros((self.contextSize, self.embedDim))
         self.wordsError = np.zeros((self.vocabSize, self.embedDim))
         self.positionsError = np.zeros((self.contextSize, self.embedDim))

mlp.py
@@ -48,8 +48,10 @@ class Mlp(Layer):
         # derivative of layer norm
         n = error.shape[-1]
         stdev = np.sqrt(self.var + 1e-5)
-        error *= self.g * (
-            (n - 1) / (n * stdev) - (self.layer2 - self.mean) ** 2 / (n * stdev**3)
+        error *= (
+            self.g
+            * (1 / n * stdev)
+            * (n - 1 - (self.layer2 - self.mean) ** 2 / self.var)
         )
         # print(error.shape)
         self.bError[1] += error.sum(0)
@@ -70,15 +72,18 @@ class Mlp(Layer):
         self.bError[0] += error.sum(0)
         # print(self.input.shape, error.shape)
         self.wError[0] += self.input.T @ error
+        # print(error.shape, self.w[0].shape)
         self.error += error @ self.w[0].T
+        # self.error += ( self.w[0] @ error.T ).T
 
     def gradientDescent(self, learningRate: float, batchSize: int):
-        self.beta -= self.betaError * learningRate / batchSize
-        self.g -= self.gError * learningRate / batchSize
-        self.b[1] -= self.bError[1] * learningRate / batchSize
+        # self.beta -= self.betaError * learningRate / batchSize
+        # self.g -= self.gError * learningRate / batchSize
+        # self.b[1] -= self.bError[1] * learningRate / batchSize
         self.w[1] -= self.wError[1] * learningRate / batchSize
-        self.b[0] -= self.bError[0] * learningRate / batchSize
-        self.w[0] -= self.wError[0] * learningRate / batchSize
+        # self.b[0] -= self.bError[0] * learningRate / batchSize
+        # self.w[0] -= self.wError[0] * learningRate / batchSize
+
         self.wError: list[np.typing.NDArray] = [
             np.zeros((self.embedDim, 4 * self.embedDim)),
             np.zeros((4 * self.embedDim, self.embedDim)),