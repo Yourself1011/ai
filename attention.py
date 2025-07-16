fix softmax derivative

attention.py
@@ -80,8 +80,11 @@ class Attention(Layer):
         self.gError += error * self.z
         # derivative of layer norm
         n = error.shape[-1]
-        stdev = np.sqrt(self.var + 1e-5)
-        error *= self.g * (1 / (n * stdev)) * (n - 1 - self.z**2)
+        stdev = np.sqrt(self.var + 1e-5).reshape((-1, 1))
+        norm = error * self.z
+        sums = norm.sum(-1).reshape((-1, 1))
+        errSums = error.sum(-1).reshape((-1, 1))
+        error = 1 / (n * stdev) * (n * error - errSums - self.z * sums)
 
         self.projError += self.combined.T @ error
 
@@ -114,6 +117,7 @@ class Attention(Layer):
 
         self.gError /= batchSize
         self.bError /= batchSize
+        self.batchSize = batchSize
 
     def gradientDescent(self, learningRate: float, t: int, mult: float):
         self.b -= self.adamW("b", self.b, self.bError, learningRate, t, mult, decay=0)
@@ -130,5 +134,5 @@ class Attention(Layer):
         self.bError: npt.NDArray = np.zeros((self.contextSize, self.embedDim))
         # self.error = np.zeros((self.contextSize, self.embedDim))
 
-        # for head in self.heads:
-        #     head.gradientDescent(learningRate, batchSize)
+        for head in self.heads:
+            head.gradientDescent(learningRate, self.batchSize)

attentionHead.py
@@ -22,7 +22,7 @@ class AttentionHead:
         self.embedDim = embedDim
         self.headCount = headCount
         self.mask = mask
-        self.tfMask = mask == 0
+        self.tfMask = mask == False
         # self.query = np.random.normal(0, 1, (embedDim, embedDim // headCount))
         # self.key = np.random.normal(0, 1, (embedDim, embedDim // headCount))
         # self.valueDown = np.random.normal(0, 1, (embedDim, embedDim // headCount))
@@ -73,19 +73,16 @@ class AttentionHead:
         self, error: npt.NDArray
     ) -> Tuple[npt.NDArray, npt.NDArray, npt.NDArray]:
         self.valueError = self.weights.T @ error
-        error = np.where(
-            self.tfMask,
-            (error @ self.value.T)
-            * self.weights
-            * (1 - self.weights)
-            / (np.sqrt(self.embedDim)),
-            0,
-        )
+
+        error = error @ self.value.T
+        sums = (error * self.weights).sum(-1).reshape((-1, 1))
+        error = self.weights * (error - sums) / np.sqrt(self.embedDim)
+
         self.queryError = error @ self.key
         self.keyError = (self.query.T @ error).T
         return self.queryError, self.keyError, self.valueError
 
-    def gradientDescent(self, learningRate: float, batchSize: int, t):
+    def gradientDescent(self, learningRate: float, batchSize: int):
         self.queryError: npt.NDArray = np.empty(
             (self.contextSize, self.embedDim // self.headCount)
         )

llm.py
@@ -252,7 +252,7 @@ class LLM(LLMBase):
         self.tokens = np.array(input[: self.contextSize + 1])
         # print("enc", time.time() - start)
         # start = time.time()
-        self.inputLength = self.tokens.size
+        self.inputLength = min(self.tokens.size, self.contextSize)
         # only the ones we input into the llm
         self.inputTokens = np.pad(
             self.tokens[: self.contextSize],
@@ -298,7 +298,7 @@ class LLM(LLMBase):
         # = s(xi) + sum(s(xj)) - 1
         # = s(xi) + 1 - 1
         # = s(xi)
-        for i in range(self.inputLength):
+        for i in range(min(self.tokens.size, self.contextSize + 1) - 1):
             error[i] = probabilities[i]
             error[i][self.tokens[i + 1]] -= 1
         # print(error.sum())
@@ -327,6 +327,8 @@ class LLM(LLMBase):
         # print(attnTime, mlpTime)
         # print(probabilities[1][self.tokens[1]])
         # print(error[1][self.tokens[1]])
+        # self.bError += error
+        # self.gError += error * self.z
 
         n = error.shape[-1]
         stdev = np.sqrt(self.var + 1e-5).reshape((-1, 1))
@@ -354,7 +356,7 @@ class LLM(LLMBase):
         self, learningRate: float, batchSize: int, t: int, clip: float = 0
     ):
         self.t = t
-        warmupSteps = 2000
+        warmupSteps = 200
         totalSteps = 600_000
         # warmupSteps = 20
         # totalSteps = 6000
@@ -393,7 +395,7 @@ class LLM(LLMBase):
         magSq += np.sum((self.gError) ** 2)
         magSq += np.sum((self.bError / batchSize) ** 2)
 
-        # print(math.sqrt(magSq))
+        print("mag:", math.sqrt(magSq), end=" ")
         if clip != 0 and magSq > clip**2:
             mult = clip / math.sqrt(magSq)
         else:
@@ -411,20 +413,16 @@ class LLM(LLMBase):
 
     def getToken(self, index: int, T: float):
         probabilities = softmax(self.a[index], T=T)
-        n = random.random()
-        i = 0
-        while n > 0:
-            n -= probabilities[i]
-            i += 1
+        i = int(np.random.choice(self.vocabSize, size=1, p=probabilities)[0])
 
-        return i - 1
+        return i
 
 
 if __name__ == "__main__":
     try:
         # with Pool(processes=1) as pool:
-        # llm = LLM(50257, 768, 1024, 12, 12)
-        llm = LLM(50257, 384, 256, 6, 6)
+        llm = LLM(50257, 768, 1024, 12, 12)
+        # llm = LLM(50257, 384, 256, 6, 6)
         start = time.time()
         try:
             llm.load()
@@ -477,9 +475,11 @@ if __name__ == "__main__":
             lastSave = time.time()
             while True:
                 llm.avgLoss = 0
-                n = math.ceil(step / 600000 * 64)
+                # n = math.ceil(step / 600000 * 64)
                 # n = round(2 ** (step / 50000 * math.log2(480)))
-                # n = 1 if step < 5000 else 480
+                # n = round(2 ** (step / 600000 * math.log2(480)))
+                n = 1 if step < 5000 else 480
+                # n = 1
                 for batch in range(n):
                     totalStart = time.time()
                     # utils.smTime = 0
@@ -521,7 +521,8 @@ if __name__ == "__main__":
                 llm.history.append([str(step), str(llm.avgLoss / n)])
 
                 start = time.time()
-                llm.gradientDescent(6e-4, n, step, clip=1)
+                # llm.gradientDescent(6e-4, n, step, clip=1)
+                llm.gradientDescent(1e-3, n, step, clip=1)
                 print("gd", time.time() - start)
                 if time.time() - lastSave > 60:
                     llm.save()

llmlayer.py
@@ -27,6 +27,7 @@ class LLMBase:
         beta2: float = 0.95,
         decay: float = 0.1,
     ):
+        # return change * lr
         if name not in self.m:
             self.m[name] = np.zeros(value.shape)
             self.v[name] = np.zeros(value.shape)
@@ -44,7 +45,6 @@ class LLMBase:
         else:
             res = m / (np.sqrt(v) + 1e-8) + decay * value
 
-        return change * lr
         return lr * res
 
 